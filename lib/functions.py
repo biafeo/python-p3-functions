fa354e5d6c219a3f150f62c411b9cd4681f0d41f
beatriz <beatrizffeo@gmail.com>
2024-04-19 12:29:17 -0600

    lab complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    pass 
 
-def greet(name):
+def greet(Guido):
+    print(f"Hello, {Guido}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    return num1+num2
 
 def halve(number):
-    pass
+    return number /2
